a2446f210bce6557bfef017e9b4fb8d8951b13d4
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-03-12 18:24:46 +0530

    server.js setup complete and listening on port 8000 with db connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,56 +1,51 @@
-import { Server } from "socket.io";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./databases/index.js"
+import bodyParser from 'body-parser';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const io = new Server(10000,{ 
-    cors: {
-        origin:"https://carsaathi-carpool.netlify.app"//react application http
-    }
+dotenv.config({ path: './.env'});
+const PORT = process.env.PORT || 8001;
+
+const app = express();
+// Middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "ERROR";
+    console.log(err);
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    })
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
-
-let users = []
-
-const addUser = (userId,socketId)=>{
-    //if user doesn't already exist in the array
-    if(!users.some((user)=>user.userId==userId)){
-       //push into users array
-       users.push({userId,socketId});
-    } 
-
-}
-
-const removeUser = (socketId)=>{
-    //socketId is different for different users
-    users = users.filter((user)=>user.socketId !== socketId)
-}
-
-const getUser = (userId)=>{
-    return users.find(user=>user.userId === userId)
-}
-
-io.on("connection", (socket) => {
-  console.log("a user connected")
-//   io.emit("welcome","you are talking to the socket server")
-
-  //take userId and socketId from user
-   socket.on("addUser",(userId)=>{
-      addUser(userId,socket.id);
-      console.log(users)
-      io.emit("getUsers",users);
-   })
-
-   //send and get message
-   socket.on("sendMessage",({senderId,receiverId,text})=>{
-    const user=getUser(receiverId)
-       io.to(user?.socketId).emit("getMessage",{
-          senderId,
-          text
-       })
-   })
-
-   //when disconnected
-   socket.on("disconnect",()=>{
-     console.log("a user disconnected!");
-     removeUser(socket.id);
-     io.emit("getUsers",users); 
-   })
-
-});
+//server listens on port 3001
+connectDB()
+    .then(() => {
+        app.on('error', (error) => {
+            console.log("Error connecting to database : " + error)
+            throw error;
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log('Server is running on port: ' + process.env.PORT)
+            console.log(`http://localhost:${process.env.PORT || 8000}`)
+        });
+        // app.listen(process.env.PORT, '0.0.0.0', () => {
+        //   console.log(`Server running at http://0.0.0.0:${process.env.PORT}`);
+        // });
+    })
+    .catch((error) => {
+        console.log("MongoDB database connection failed!!!: " + error)
+        throw error;
+    })
+// /Users/keshavthakur/Desktop/NeedNear/server/index.js
