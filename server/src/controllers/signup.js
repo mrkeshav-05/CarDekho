7bd16333f0d1bae5bd44ceacaf58702a0dfbdf27
Rishabh Kushwaha <remonal@Rishabhs-MacBook-Air.local>
2025-03-13 21:44:21 +0530

    Signup controller update

--- server/src/controllers/signup.js
+++ server/src/controllers/signup.js
@@ -55,3 +55,49 @@ export const forgetPassword = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+//add reset password
+export const resetPassword = async (req, res) => {
+  const { token } = req.params;
+  const password = req.query.password
+  console.log('password: ',password)
+  try {
+    const decoded = jwt.verify(token, "3S1KeYwhichISUn1@u3>>**CArS@@Th1", (err, decode) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return decode;
+      }
+    });
+    const id = decoded.id;
+    // console.log(id);
+    const user = await User.findById(id);
+    if (!user) {
+      return res.json({ message: "Invalid User" }).status(400);
+    }
+    // console.log(user)
+    // console.log(user.resetToken)
+    const resetToken = user.resetToken;
+    const resetTokenExpiration = user.resetTokenExpiration;
+    if (Date.now() > resetTokenExpiration) {
+      return res.json({ message: "Token Expired" }).status(400);
+    }
+    // console.log("token: ",token);
+    // console.log("resetToken: ",resetToken);
+    if (token !== resetToken) {
+      return res.status(401).send("Invalid token");
+    }
+    const salt = bcrypt.genSaltSync(10);
+    // console.log(req.body);
+    const hash = bcrypt.hashSync(password, salt);
+    const updateUser = await User.findByIdAndUpdate(id, {
+      password: hash,
+      resetToken: "hihihi",
+      resetTokenExpiration: new Date(),
+    });
+    return res.json({ message: "Password Updated", updateUser}).status(200);
+  } catch (error) {
+    console.error("Failed to verify token:", error);
+    return res.status(401).send("Invalid toke last walo bhai");
+  }
+};
