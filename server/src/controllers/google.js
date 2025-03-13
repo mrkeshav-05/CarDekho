16234a8d28be9e7f60616b9f0c867a94addad59f
Rishabh Kushwaha <remonal@Rishabhs-MacBook-Air.local>
2025-03-13 22:14:57 +0530

    add edit trip

---
+++ server/src/controllers/google.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import User from  "../models/user.js"
+import bcrypt from "bcryptjs";
+import  jwt  from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import { validationResult } from "express-validator";
+
+export const googleAuth = async (req, res, next) => {
+    try {
+      console.log("noice",req.body);
+      const user = await User.findOne({ email: req.body.email });
+      if (user) {
+        res
+          .status(200)
+          .json(user._doc);
+      } else {
+        const newUser = new User({
+          name:req.body.username,
+          ...req.body,
+        });
+        const savedUser = await newUser.save();
+        res
+          .status(200)
+          .json(savedUser._doc);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
