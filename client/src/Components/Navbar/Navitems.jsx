bf6e4c592b039b9047126a15201c63ae36e92401
Rishabh Kushwaha <remonal@Rishabhs-MacBook-Air.local>
2025-03-15 15:57:48 +0530

    Navitem add

---
+++ client/src/Components/Navbar/Navitems.jsx
@@ -0,0 +1,45 @@
+export const navItems = [
+    {
+      
+      id: 1,
+      title: "Find A Ride",
+      path: "/search",
+      cName: "nav-item",
+    },
+    {
+      
+      id: 2,
+      title: "Rides",
+      path: "/myrides",
+      cName: "nav-item",
+    },
+    {
+      id: 4,
+      title: "Bookings",
+      path: '/myBooking',
+      cName: "nav-item",
+    },
+    {
+      id: 5,
+      title: "Publish Ride",
+      path: '/createtrip',
+      cName: "nav-item",
+    },
+    {
+      id: 6,
+      title: "Messenger",
+      path: "/messenger",
+      cName: "submenu-item",
+      
+    },
+    {
+      id: 3,
+      title: "Contact Us",
+      path: "/contactus",
+      cName: "nav-item",
+      
+    },
+  ];
+  
+  
+  
