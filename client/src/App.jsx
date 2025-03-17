c163d7b4e27add0058b70a22fc2cf5b94de0a622
Rishabh Kushwaha <goldenyaro4321@gmail.com>
2025-03-18 03:12:24 +0530

    Add routing for ContactUs component in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import { useState, useEffect } from 'react';
+import { ContactUs } from './pages/ContactUs/contact.jsx';
+
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || {});
@@ -16,6 +18,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/contactus" element={<ContactUs user={user} setIsLoggedIn={setIsLoggedIn} />} />
+
         <Route path="/" element={<Dashboard user={user} setUser={setUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
       </Routes>
     </BrowserRouter>
