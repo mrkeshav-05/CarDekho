41473ea45e05b9246fddab639e0322a5f9e41cc4
Rishabh Kushwaha <goldenyaro4321@gmail.com>
2025-03-18 03:00:36 +0530

    Add Analytics components with responsive design and engaging content

---
+++ client/src/components/Analytics/Analytics.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import carImage from "../../Images/4.png"; // Import the image
+
+function Analytics() {
+  return (
+    <>
+      <div className="w-full bg-white py-0 px-10">
+        <div className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 items-up">
+        
+          <div className="text-center md:text-left mt-8 md:mt-0 p-6 flex flex-col justify-center">
+            <p className="text-yellow-500 font-bold font-serif text-lg md:text-xl mb-4">
+              Discover Your Next Adventure✨
+            </p>
+            <h1 className="text-[#171717] md:text-5xl sm:text-4xl text-3xl font-bold mb-4 font-serif leading-tight">
+              Find the Cheapest Rides Anywhere
+            </h1>
+            <p className="text-justify pr-8 text-base md:text-md font-serif mb-6 text-[#171717">
+              Are you ready to embark on unforgettable journeys without breaking
+              the bank? With our platform, discover the most affordable rides to
+              your dream destinations. Say yes to adventures!
+            </p>
+          </div>
+          <img
+            src={carImage}
+            alt="Spreadsheet Illustration"
+            className="w-[90%] h-[80%] my-4 rounded-md ml-24"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Analytics;
---
+++ client/src/components/Analytics/Analytics2.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import carImage from "../../Images/3.png"; // Import the image
+
+
+function Analytics() {
+  return (
+    <>
+      <div className="w-full bg-[#f2f2f2] py-7 px-14">
+        <div className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 items-up">
+        <img
+            src={carImage}
+            alt="Car Pooling Illustration"
+            className="w-[80%] h-[90%] my-4 rounded-md md:mr-6"
+          />
+          <div className="text-center md:text-left text-[#171717] mt-8 flex flex-col justify-center">
+          <p className="text-yellow-500 font-bold font-serif text-lg md:text-xl mb-4">
+              Explore new possibilities✨
+            </p>
+            <h1 className="text-[#171717] md:text-5xl sm:text-4xl text-3xl font-bold mb-4 font-serif leading-tight">
+              Find Your Perfect Ride
+            </h1>
+            <p className="text-justify pr-8 text-base md:text-md font-serif mb-6">
+              Ready to embark on a journey of convenience and affordability? Our
+              platform connects you with the best rides tailored to your needs.
+              Say goodbye to travel hassles and hello to seamless adventures!
+            </p>
+          </div>
+         
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Analytics;
