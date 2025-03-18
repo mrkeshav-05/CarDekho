42cb48dfd0f1c85c7b07239f6d6a1059806450cf
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-03-18 18:59:12 +0530

    remove Firebase configuration to use environment variables for sensitive data

--- client/src/pages/firebase.js
+++ client/src/pages/firebase.js
@@ -3,14 +3,23 @@ import {getAuth,GoogleAuthProvider} from "firebase/auth"
 import { getStorage } from "firebase/storage";
 import 'firebase/storage'; 
 
+const FIREBASE_API_KEY = import.meta.FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = import.meta.FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = import.meta.FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = import.meta.FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = import.meta.FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = import.meta.FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = import.meta.FIREBASE_MEASUREMENT_ID;
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA1bM5UOnxGXnSRaX-CK8EKaFrnEkXFnkA",
-  authDomain: "cardekho-f0892.firebaseapp.com",
-  projectId: "cardekho-f0892",
-  storageBucket: "cardekho-f0892.firebasestorage.app",
-  messagingSenderId: "750229060208",
-  appId: "1:750229060208:web:40ac696f85dc7eccf2d2b5",
-  measurementId: "G-357J73LHSW"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
