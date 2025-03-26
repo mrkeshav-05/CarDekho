4f6cb557ae558d811c705403b209e97102f2341c
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-03-27 00:17:35 +0530

    message controller for improved

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -3,48 +3,48 @@ import Conversation from '../models/conversation.model.js';
 // import { getReceiverSocketId, io} from '../socket/socket.js';
 
 const getReceiverSocketId = (hi) => {
-    return 'hi';
+	return 'hi';
 }
 //New Conversation
 export const sendMessages = async (req, res) => {
-    try {
-        const { message } = req.body;
+	try {
+		const { message } = req.body;
 		const { id: receiverId } = req.params;
 		const senderId = req.user._id;
-        let conversation = await Conversation.findOne({
+		let conversation = await Conversation.findOne({
 			participants: { $all: [senderId, receiverId] },
 		});
-        if (!conversation) {
+		if (!conversation) {
 			conversation = await Conversation.create({
 				participants: [senderId, receiverId],
 			});
 		}
-        const newMessage = new Message({
+		const newMessage = new Message({
 			senderId,
 			receiverId,
 			message,
 		});
-        if (newMessage) {
+		if (newMessage) {
 			conversation.messages.push(newMessage._id);
 		}
 		await Promise.all([conversation.save(), newMessage.save()]);
-        // Emitting new message to the recipient's socket
-        const receiverSocketId = getReceiverSocketId(receiverId);
+		// Emitting new message to the recipient's socket
+		const receiverSocketId = getReceiverSocketId(receiverId);
 		if (receiverSocketId) {
 			// io.to(<socket_id>).emit() used to send events to specific client
 			io.to(receiverSocketId).emit("newMessage", newMessage);
 		}
 		res.status(201).json(newMessage);
-    } catch (error) {
-        console.log("Error in sendMessage controller: ", error.message);
+	} catch (error) {
+		console.log("Error in sendMessage controller: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
-    }
+	}
 }
 
 //get
-export const getMessages = async(req,res)=>{
-    try {
-        const { id: userToChatId } = req.params;
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params;
 		const senderId = req.user._id;
 
 		const conversation = await Conversation.findOne({
@@ -56,8 +56,8 @@ export const getMessages = async(req,res)=>{
 		const messages = conversation.messages;
 
 		res.status(200).json(messages);
-    } catch (error) {
-        console.log("Error in getMessages controller: ", error.message);
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
-    }
+	}
 }
