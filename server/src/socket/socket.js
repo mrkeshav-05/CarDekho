b8bf18ef0c01663466fd323f6923ad2c4684f811
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-03-31 16:49:04 +0530

    Update socket CORS configuration to use frontend URL from environment variable

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -27,12 +27,13 @@ import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
 import express from "express";
+const frontendUrl = process.env.FRONTEND_URL;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Change this to your frontend URL
+    origin: frontendUrl, // Change this to your frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
